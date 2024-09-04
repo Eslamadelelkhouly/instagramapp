3af8d38c5a0cdf3c4f6c394a09efe54593d12a0f
Eslamadelelkhouly <rodyerak@gmail.com>
2024-09-04 14:46:39 +0300

    add text fotter post

--- lib/features/home/presentation/views/widgets/fotter_post.dart
+++ lib/features/home/presentation/views/widgets/fotter_post.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:instagramapp/features/home/presentation/views/widgets/icon_fotter_post.dart';
+import 'package:instagramapp/features/home/presentation/views/widgets/text_fotter.dart';
 
 class FotterPost extends StatelessWidget {
   const FotterPost({super.key});
@@ -7,9 +8,10 @@ class FotterPost extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         const IconFotterPost(),
-        
+        const TextFotter(),
       ],
     );
   }
--- lib/features/home/presentation/views/widgets/text_fotter.dart
+++ lib/features/home/presentation/views/widgets/text_fotter.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:instagramapp/core/utils/style.dart';
 
 class TextFotter extends StatelessWidget {
   const TextFotter({super.key});
@@ -6,10 +7,31 @@ class TextFotter extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Text(
           '280 likes',
-          
+          style: Style.textStyle15.copyWith(
+            color: Colors.white,
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(left: 10, top: 2),
+          child: Text(
+            'nice photo today',
+            style: Style.textStyle15.copyWith(
+              color: Colors.white,
+            ),
+          ),
+        ),
+        TextButton(
+          onPressed: () {},
+          child: Text(
+            'Add comment',
+            style: Style.textStyle15.copyWith(
+              color: Colors.white,
+            ),
+          ),
         ),
       ],
     );

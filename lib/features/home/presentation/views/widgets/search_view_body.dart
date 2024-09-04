fcb8ea13b5cc397ee6aecb517b75648bfb9fc6ce
Eslamadelelkhouly <rodyerak@gmail.com>
2024-09-04 15:58:41 +0300

    add text field to search body

--- lib/features/home/presentation/views/widgets/search_view_body.dart
+++ lib/features/home/presentation/views/widgets/search_view_body.dart
@@ -1,13 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:instagramapp/core/utils/border.dart';
 
-class SearchViewBody extends StatelessWidget {
+class SearchViewBody extends StatefulWidget {
   const SearchViewBody({super.key});
 
   @override
+  State<SearchViewBody> createState() => _SearchViewBodyState();
+}
+
+class _SearchViewBodyState extends State<SearchViewBody> {
+  final SearchController = TextEditingController();
+
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text(
-        'Search Screen',
+    return SafeArea(
+      child: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(10),
+          child: Column(
+            children: [
+              TextField(
+                controller: SearchController,
+                decoration: InputDecoration(
+                  enabledBorder: customborderenable(),
+                  focusedBorder: customborderfoucs(),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }

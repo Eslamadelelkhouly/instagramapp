49e9bd28f6290c831fe9f5316e829d621307f90c
Eslamadelelkhouly <rodyerak@gmail.com>
2024-09-05 18:19:12 +0300

    add comment screen

---
+++ lib/features/comment/presentation/views/widgets/comment_view_body.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CommentViewBody extends StatelessWidget {
+  const CommentViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column();
+  }
+}

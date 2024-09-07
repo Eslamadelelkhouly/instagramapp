3bd4281fee6a3c1935c41bb77c9c2773cb93f540
Eslamadelelkhouly <rodyerak@gmail.com>
2024-09-08 02:06:03 +0300

    make streambuilder on main to know state user login

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,13 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:instagramapp/features/home/presentation/views/home_screen.dart';
 import 'package:instagramapp/features/signUp/presentation/view/signup_screen.dart';
 import 'package:instagramapp/firebase_options.dart';
 
 void main() async {
-  WidgetsFlutterBinding
-      .ensureInitialized();
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
@@ -25,7 +26,24 @@ class MyApp extends StatelessWidget {
         scaffoldBackgroundColor: Colors.black,
         fontFamily: GoogleFonts.firaSans().fontFamily,
       ),
-      home: const SignupScreen(),
+      home: StreamBuilder(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return CircularProgressIndicator();
+          }
+          if (snapshot.hasError) {
+            return Text('error');
+          }
+          if (snapshot.data == null) {
+            return SignupScreen();
+          }
+          if (snapshot.hasData) {
+            return HomeScreen();
+          }
+          return Text('Has an error');
+        },
+      ),
     );
   }
 }

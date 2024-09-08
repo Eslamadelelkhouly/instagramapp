3992c403a5be08b977311a0689bed6fc818e5843
Eslamadelelkhouly <rodyerak@gmail.com>
2024-09-08 18:45:38 +0300

    create class provider user

--- lib/features/home/presentation/manager/provider_user.dart
+++ lib/features/home/presentation/manager/provider_user.dart
@@ -1,1 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:instagramapp/features/home/data/models/user_model.dart';
+import 'package:instagramapp/features/home/function/firebase/firestore.dart';
 
+class ProviderUser with ChangeNotifier {
+  UserModel? userdata;
+  UserModel? get getUser {
+    return userdata;
+  }
+
+  void fetchUser() async {
+    UserModel user = await Firestore().UserDetails();
+    userdata = user;
+    notifyListeners();
+  }
+}

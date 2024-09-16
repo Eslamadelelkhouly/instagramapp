bb35a822065c9377ab7a12717abb2e239cab68ba
Eslamadelelkhouly <rodyerak@gmail.com>
2024-09-17 02:47:41 +0300

    add video show in story uplode

--- lib/features/home/presentation/views/widgets/add_status_view_body.dart
+++ lib/features/home/presentation/views/widgets/add_status_view_body.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:video_player/video_player.dart';
 import 'package:instagramapp/features/home/presentation/views/widgets/choice_image_video.dart';
 import 'package:instagramapp/features/home/presentation/views/widgets/title_screen_add.dart';
 
@@ -12,11 +13,18 @@ class AddStatusViewBody extends StatefulWidget {
 }
 
 class _AddViewBodyState extends State<AddStatusViewBody> {
+  VideoPlayerController? videocontroller;
   File? pickimage;
   File? pickvideo;
   bool _isPickingimage = false;
   bool _isPickvideo = false;
   final description = TextEditingController();
+
+  @override
+  void dispose() {
+    videocontroller?.dispose(); // Dispose the video controller when not needed
+    super.dispose();
+  }
 
   Future<void> selectImage() async {
     if (_isPickingimage) return;
@@ -33,6 +41,7 @@ class _AddViewBodyState extends State<AddStatusViewBody> {
         setState(() {
           pickimage = File(image.path);
           pickvideo = null;
+          videocontroller?.pause(); // Pause video if an image is selected
         });
       }
     } catch (e) {
@@ -56,13 +65,18 @@ class _AddViewBodyState extends State<AddStatusViewBody> {
         source: ImageSource.gallery,
       );
       if (video != null) {
-        setState(() {
-          pickvideo = File(video.path);
-          pickimage = null;
-        });
+        pickvideo = File(video.path);
+        pickimage = null;
+
+        // Initialize the video player
+        videocontroller = VideoPlayerController.file(pickvideo!)
+          ..initialize().then((_) {
+            setState(() {}); // Update the UI once the video is initialized
+            videocontroller?.play();
+          });
       }
     } catch (e) {
-      print('Error picking image: $e');
+      print('Error picking video: $e');
     } finally {
       setState(() {
         _isPickvideo = false;
@@ -85,6 +99,8 @@ class _AddViewBodyState extends State<AddStatusViewBody> {
                 onPressedcancel: () {
                   setState(() {
                     pickimage = null;
+                    pickvideo = null;
+                    videocontroller?.pause();
                     description.text = '';
                   });
                   Navigator.of(context).pop();
@@ -92,7 +108,26 @@ class _AddViewBodyState extends State<AddStatusViewBody> {
                 onPressednext: () {},
               ),
               pickimage == null
-                  ? SizedBox(height: height * 0.4)
+                  ? pickvideo == null
+                      ? SizedBox(height: height * 0.4)
+                      : Container(
+                          width: double.infinity,
+                          height: 500,
+                          child: videocontroller != null &&
+                                  videocontroller!.value.isInitialized
+                              ? AspectRatio(
+                                  aspectRatio:
+                                      videocontroller!.value.aspectRatio,
+                                  child: VideoPlayer(videocontroller!),
+                                )
+                              : Center(
+                                  child: SizedBox(
+                                    width: 50,
+                                    height: 50, // Small circular progress size
+                                    child: const CircularProgressIndicator(),
+                                  ),
+                                ), // Show a loading indicator while the video is initializing
+                        )
                   : Image.file(
                       pickimage!,
                       height: height * 0.4,
